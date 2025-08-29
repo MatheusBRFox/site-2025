bf94e6d5430d096a280912a2b81bd2e99dafa63d
MatheusBRFox <matheus-2951885@estudante.rs.gov.br>
2025-08-29 08:40:53 -0300

    continuação do formulario

--- portal/conexao.php
+++ portal/conexao.php
@@ -19,5 +19,5 @@
     mysqli_query($conexao, "SET NAMES 'utf8'");
     mysqli_query($conexao, 'SET character_set_connection=utf8');
     mysqli_query($conexao, 'SET character_set_client=utf8');
-    mysqli_query($conexao, 'SET character_set_rerults=utf8');
+    mysqli_query($conexao, 'SET character_set_results=utf8');
 ?>
--- portal/formplano.php
+++ portal/formplano.php
@@ -15,26 +15,25 @@
 <br>
     <center><h5> PREENCHA OS CAMPOS ABAIXO PARA ADICIONAR UM NOVO PLANO </h5></center>
 <br>
-<form>
+<form action="formplano_recebe.php">
     <div class="form-row">
         <div class="col">
             <label for="nomePlano">* Nome</label>
-            <input type="text" class="form-control" placeholder="Nome do plano">
+            <input type="text" class="form-control" name="nome_plano" id="nome_plano" placeholder="Nome do plano">
     </div>
     <div class="col">
         <label for="descricaoPlano">* Descrição</label>
-        <input type="text" class="form-control" placeholder="Descreva detalhes do plano">
+        <input type="text" class="form-control" name="descricao_plano" id="descricao_plano" placeholder="Descreva detalhes do plano">
     </div>
         <div class="col">
             <label for="valorPlano">* Valor</label>
-            <input type="text" class="form-control" placeholder="R$ 0,00">
+            <input type="text" class="form-control" name="valor_plano" id="valor_plano" placeholder="R$ 0,00">
         </div>
     </div>
     <br>
     <div class="col-12">
             <button type="submit" class="btn btn-primary" name="send" id="send ">Enviar</button>
             <button type="button" class="btn btn-danger" name="limpa" id="limpa" onclick='form.reset()'>
-                <i class='fa fa-eraser' style="font-size:25px;"</i>
                 Limpar
             </button>
     </div>
